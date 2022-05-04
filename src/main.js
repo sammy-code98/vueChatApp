5afd827b31eda823013116b53fc32f0367645c21
sammy-code98 <sammysmart98@yahoo.com>
2022-05-04 16:35:01 +0100

    feat:install and configure firebase

--- src/main.js
+++ src/main.js
@@ -1,6 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import { initializeApp } from "firebase/app";
 
-createApp(App).use(store).use(router).mount('#app')
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBP-Qh4vje-zx4M6H2GET6Dd66P1IMJmIk",
+  authDomain: "vuechat-50d8e.firebaseapp.com",
+  projectId: "vuechat-50d8e",
+  storageBucket: "vuechat-50d8e.appspot.com",
+  messagingSenderId: "368362954978",
+  appId: "1:368362954978:web:d8c38cfefd47dcc6abbebb",
+};
+
+// Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+createApp(App).use(store).use(router).mount("#app");
